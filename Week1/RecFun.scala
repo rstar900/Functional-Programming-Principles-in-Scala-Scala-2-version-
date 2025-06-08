4b58feb9062a03d478c5528ae51f6a2c249a605e
Rachit Garg <rachitgarg900@gmail.com>
2025-06-08 17:42:22 +0200

    Added solutions till Week 1 exercise 2

---
+++ Week1/RecFun.scala
@@ -0,0 +1,52 @@
+package recfun
+
+object RecFun extends RecFunInterface {
+
+  def main(args: Array[String]): Unit = {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(s"${pascal(col, row)} ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = 
+    if (c == 0 || c == r) 1 else pascal(c-1, r-1) + pascal(c, r-1)
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    def balanceHelper(parentheses_list: List[Char], chars_itr: List[Char]): Boolean =
+      // Base case
+      if (chars_itr.isEmpty) 
+        if (parentheses_list.isEmpty)
+          true else false
+      // recursive case
+      // We are only considering '(' and ')'
+      else if (parentheses_list.isEmpty && chars_itr.head == ')')
+        // Nothing to pop, means already unbalanced
+        false
+      else if (!parentheses_list.isEmpty && chars_itr.head == ')' && parentheses_list.head =='(')
+        // Pop the '('
+        balanceHelper(parentheses_list.tail, chars_itr.tail) 
+      else if (chars_itr.head =='(')
+        // Add the '(' to the head
+        balanceHelper('(' +: parentheses_list, chars_itr.tail)
+      else
+        // Otherwise just ignore and move ahead
+       balanceHelper(parentheses_list, chars_itr.tail)
+
+     // Function application  
+     balanceHelper("".toList, chars)
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
